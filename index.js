236a89f3b8f6cc480a2545fb3c173272178d70b7
Seth MacPherson <macpherson.seth@gmail.com>
2020-08-27 17:02:40 -0700

    Last project solved. Looking over sprint

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const burger = {name: "Burger", price: 18, category: "Lunch"};
 const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakfast"};
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
+let menuTotal = [latte, burger, breakfastBurrito]
 
 function createMenuItem(name, cost, category){
-    
+  let newMenuItem = {name, cost, category} 
+    return newMenuItem;
 }
+console.log(createMenuItem("Taco", 14, "Lunch"));
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
-
+console.log(createMenuItem("Pizza", 14, "Dinner"));
+console.log(createMenuItem("Ice Cream", 6, "Dessert"));
+console.log(createMenuItem("Taco", 12, "Lunch"));
 
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
@@ -22,7 +27,40 @@ Your method should accept:
 
 and should return a number. 
 
-For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
+For example, burgerDiscount("teacher") would return 13.5 and burgerDiscount("public") would return 16.2*/
+
+const discountTable = [
+  {type: "teacher" , cost: 0.75 },
+  {type: "student" , cost: 0.90 },
+  {type: "public"  , cost: 1.00 }
+]
+
+function burgerDiscount(myRole){
+  for (let i = 0; i < discountTable.length; i++){
+    if (discountTable[i].type === myRole){
+      let newPrice = burger.price * discountTable[i].cost;
+      return newPrice;
+    }
+  }
+}
+console.log(burgerDiscount('teacher'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -42,7 +80,8 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 console.log(reviews[5].feedback);
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
-
+reviews.push({name: "Seth", rating: 5, feedback: "Best place I would never come back to"});
+console.log(reviews[reviews.length-1]);
 
 // Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
 reviews[6].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
@@ -60,10 +99,15 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * For example, if getReviewByIndex is invoked with reviews and the number 0
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
+
+//sample code from Nathan's codepen
+// console.log(`Hi, my name is ${this.name}`)
+
+
 function getReviewByIndex(reviews, index) {
-    /* code here */
+  return `${reviews[index].name} gave the restaurant a ${reviews[index].rating}, and their feedback was: ${reviews[index].feedback}`
   }
-  
+console.log(getReviewByIndex ( reviews , 0 ));
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
@@ -74,9 +118,29 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
+function getLastReview(reviews) {
+    let lastReview = reviews.length - 1;
+    return `${reviews[lastReview].name} gave the restaurant a ${reviews[lastReview].rating}, and their feedback was: ${reviews[lastReview].feedback}`
   } 
+  console.log(getLastReview(reviews));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
